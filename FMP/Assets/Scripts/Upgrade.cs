f93219507cf758add5bda3a5812ea7308097e062
Easlee Liu <easlee@outlook.com>
2022-08-22 01:13:58 +0800

    support update

--- FMP/Assets/Scripts/Upgrade.cs
+++ FMP/Assets/Scripts/Upgrade.cs
@@ -7,6 +7,7 @@ using UnityEngine.Networking;
 using System;
 using static Upgrade.Schema;
 using Newtonsoft.Json;
+using static UpgradeBehaviour;
 
 public class Upgrade
 {
@@ -15,7 +16,7 @@ public class Upgrade
         public class Entry
         {
             public string file = "";
-            public long size = 0;
+            public ulong size = 0;
             public string hash = "";
         }
         public List<Entry> entries = new List<Entry>();
@@ -23,6 +24,15 @@ public class Upgrade
 
     public class Schema
     {
+        public class Option
+        {
+            [XmlAttribute("attribute")]
+            public string attribute { get; set; } = "";
+
+            [XmlAttribute("values")]
+            public string values { get; set; } = "";
+        }
+
         public class Reference
         {
             [XmlAttribute("org")]
@@ -44,7 +54,7 @@ public class Upgrade
         public class FMP
         {
             [XmlAttribute]
-            public string environment { get; set; } = "";
+            public string environment { get; set; } = "product";
             [XmlAttribute]
             public string repository { get; set; } = "";
             [XmlArray("References"), XmlArrayItem("Reference")]
@@ -56,20 +66,39 @@ public class Upgrade
 
         public class Update
         {
+            [XmlAttribute("strategy")]
+            public string strategy { get; set; } = "skip";
             [XmlElement("FMP")]
             public FMP fmp { get; set; } = new FMP();
         }
 
-        [XmlElement("Update")]
-        public Update update { get; set; } = new Update();
+        public class Body
+        {
+
+            [XmlElement("Update")]
+            public Update update { get; set; } = new Update();
+        }
+
+        [XmlElement("Body")]
+        public Body body { get; set; } = new Body();
+
+        [XmlArray("Header"), XmlArrayItem("Option")]
+        public Option[] options { get; set; } = new Option[] {
+            new Option
+            {
+                attribute = "Update.strategy",
+                values = "升级策略，可选值为：skip, auto, manual",
+            },
+        };
     }
 
     public class FileTask
     {
         public string url { get; set; }
-        public long size { get; set; }
+        public ulong size { get; set; }
         public string hash { get; set; }
         public string saveAs { get; set; }
+        public bool finished { get; set; } = false;
     }
 
     public class ReferenceTask
@@ -80,78 +109,93 @@ public class Upgrade
         public string md5json_url { get; set; }
         public List<FileTask> files = new List<FileTask>();
         public bool parsed { get; set; } = false;
+        public bool finished { get; set; } = false;
     }
-
-    public bool DependenciesHasUpdate { get; private set; }
 
     public enum ErrorCode
     {
         OK,
-        NETWORK_ERROR,
-        PARSE_ERROR,
-        NOTFOUND_ERROR,
+        MANIFEST_NETWORK_ERROR,
+        MANIFEST_PARSE_ERROR,
+        ENTRY_NOTFOUNDINREPO,
+        ENTRY_NETWORK_ERROR,
+        ENTRY_SIZE_ERROR,
     }
 
-    public class Result
+
+    public ErrorCode errorCode { get; private set; }
+    public ulong updateTotalSize { get; private set; } = 0;
+    public ulong updateFinishedSize { get; private set; } = 0;
+    public string updateEntryHash { get; private set; } = "";
+    public float updateEntryProgress
     {
-        public long totalSize { get; set; }
+        get
+        {
+            if (null == fileWebRequest_)
+                return 0;
+            return fileWebRequest_.downloadProgress;
+        }
     }
 
-
-    private Queue<ReferenceTask> referenceTaskQueue_ = new Queue<ReferenceTask>();
-
-    public ErrorCode updateDependenciesError { get; private set; }
-    public Result result { get; private set; }
+    private List<ReferenceTask> referenceTaskQueue_ = new List<ReferenceTask>();
+    private UnityWebRequest fileWebRequest_ = null;
 
     public void ParseSchema(Schema _schema)
     {
         referenceTaskQueue_.Clear();
-        foreach (var reference in _schema.update.fmp.refenrences)
+        foreach (var reference in _schema.body.update.fmp.refenrences)
         {
             ReferenceTask task = new ReferenceTask();
             string version = reference.version;
-            if (_schema.update.fmp.environment.Equals("develop"))
+            if (_schema.body.update.fmp.environment.Equals("develop"))
                 version = "develop";
 
-            string address = string.Format("{0}/{1}/{2}@{3}", _schema.update.fmp.repository, reference.org, reference.module, version);
+            string address = string.Format("{0}/{1}/{2}@{3}", _schema.body.update.fmp.repository, reference.org, reference.module, version);
             task.org = reference.org;
             task.module = reference.module;
             task.version = version;
             task.md5json_url = string.Format("{0}/md5.json", address);
             FileTask bridgeFileTask = new FileTask();
             bridgeFileTask.url = string.Format("{0}/fmp-{1}-{2}-lib-bridge.dll", address, reference.org.ToLower(), reference.module.ToLower());
-            bridgeFileTask.saveAs = string.Format("modules/fmp-{1}-{2}-lib-bridge.dll", address, reference.org.ToLower(), reference.module.ToLower());
+            bridgeFileTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-bridge.dll", reference.org.ToLower(), reference.module.ToLower());
             task.files.Add(bridgeFileTask);
             FileTask protoFileTask = new FileTask();
             protoFileTask.url = string.Format("{0}/fmp-{1}-{2}-lib-proto.dll", address, reference.org.ToLower(), reference.module.ToLower());
-            protoFileTask.saveAs = string.Format("modules/fmp-{1}-{2}-lib-proto.dll", address, reference.org.ToLower(), reference.module.ToLower());
+            protoFileTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-proto.dll", reference.org.ToLower(), reference.module.ToLower());
             task.files.Add(protoFileTask);
             FileTask mvcsFileTask = new FileTask();
             mvcsFileTask.url = string.Format("{0}/fmp-{1}-{2}-lib-mvcs.dll", address, reference.org.ToLower(), reference.module.ToLower());
-            mvcsFileTask.saveAs = string.Format("modules/fmp-{1}-{2}-lib-mvcs.dll", address, reference.org.ToLower(), reference.module.ToLower());
+            mvcsFileTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-mvcs.dll", reference.org.ToLower(), reference.module.ToLower());
             task.files.Add(mvcsFileTask);
             FileTask unityFileTask = new FileTask();
             unityFileTask.url = string.Format("{0}/{1}.FMP.MOD.{2}.LIB.Unity.dll", address, reference.org, reference.module);
-            unityFileTask.saveAs = string.Format("modules/{0}.FMP.MOD.{1}.LIB.Unity.dll", address, reference.org, reference.module);
+            unityFileTask.saveAs = string.Format("modules/{0}.FMP.MOD.{1}.LIB.Unity.dll", reference.org, reference.module);
             task.files.Add(unityFileTask);
             FileTask uabFileTask = new FileTask();
-            uabFileTask.url = string.Format("{0}/{1}_{2}@win32.uab", address, reference.org.ToLower(), reference.module.ToLower());
-            uabFileTask.saveAs = string.Format("uabs/{0}_{1}@win32.uab", address, reference.org.ToLower(), reference.module.ToLower());
+            uabFileTask.url = string.Format("{0}/{1}_{2}{3}.uab", address, reference.org.ToLower(), reference.module.ToLower(), getPlatformSuffix());
+            uabFileTask.saveAs = string.Format("uabs/{0}_{1}.uab", reference.org.ToLower(), reference.module.ToLower());
             task.files.Add(uabFileTask);
-            referenceTaskQueue_.Enqueue(task);
+            referenceTaskQueue_.Add(task);
         }
     }
 
     public IEnumerator CheckDependencies(Schema _schema)
     {
-        updateDependenciesError = ErrorCode.OK;
-        result = new Result();
+        errorCode = ErrorCode.OK;
+        updateTotalSize = 0;
+        updateFinishedSize = 0;
+        updateEntryHash = "";
+
+        string cache_dir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
+        cache_dir = Path.Combine(cache_dir, ".upgrade");
+
         foreach (var task in referenceTaskQueue_)
         {
+            //解析过的包不需要再次清单文件
             if (task.parsed)
                 continue;
 
-            // 下载MD5
+            // 下载包的清单文件
             UnityLogger.Singleton.Debug("pull {0}", task.md5json_url);
             using (UnityWebRequest uwr = UnityWebRequest.Get(task.md5json_url))
             {
@@ -160,11 +204,12 @@ public class Upgrade
                 if (uwr.result != UnityWebRequest.Result.Success)
                 {
                     UnityLogger.Singleton.Error(uwr.error);
-                    updateDependenciesError = ErrorCode.NETWORK_ERROR;
+                    errorCode = ErrorCode.MANIFEST_NETWORK_ERROR;
                     yield break;
                 }
-                // 解析MD5
+                // 解析清单文件
                 string text = uwr.downloadHandler.text;
+
                 MD5Json md5json = new MD5Json();
                 try
                 {
@@ -172,9 +217,15 @@ public class Upgrade
                 }
                 catch (Exception ex)
                 {
-                    UnityLogger.Singleton.Error(ex.Message);
                     UnityLogger.Singleton.Exception(ex);
-                    updateDependenciesError = ErrorCode.PARSE_ERROR;
+                    errorCode = ErrorCode.MANIFEST_PARSE_ERROR;
+                    yield break;
+                }
+
+                if (null == md5json)
+                {
+                    UnityLogger.Singleton.Error("manifest is null, maybe convert from json is failed");
+                    errorCode = ErrorCode.MANIFEST_PARSE_ERROR;
                     yield break;
                 }
 
@@ -184,17 +235,165 @@ public class Upgrade
                     {
                         return _item.file.Equals(Path.GetFileName(filetask.url));
                     });
+                    // 任何一个需要更新的文件，如果在仓库中找不到，都报错
                     if (null == entry)
                     {
-                        updateDependenciesError = ErrorCode.NOTFOUND_ERROR;
+                        errorCode = ErrorCode.ENTRY_NOTFOUNDINREPO;
                         yield break;
                     }
                     filetask.size = entry.size;
                     filetask.hash = entry.hash;
-                    result.totalSize += entry.size;
+                    filetask.finished = false;
+                    // 如果文件已经下载
+                    string hashFile = Path.Combine(cache_dir, filetask.saveAs) + ".hash";
+                    if (File.Exists(hashFile))
+                    {
+                        if (File.ReadAllText(hashFile).Equals(filetask.hash))
+                        {
+                            filetask.finished = true;
+                        }
+                    }
                 }
                 task.parsed = true;
             }
         }
+
+        // 重新计算需要下载总量和已下载量
+        foreach (var reference in referenceTaskQueue_)
+        {
+            foreach (var file in reference.files)
+            {
+                updateTotalSize += file.size;
+                if (file.finished)
+                    updateFinishedSize += file.size;
+            }
+        }
     }
-  }
+
+    public IEnumerator DownloadDependencies(Schema _schema)
+    {
+        errorCode = ErrorCode.OK;
+        yield return downloadDependencies(_schema);
+    }
+
+    public IEnumerator OverwriteDependencies(Schema _schema)
+    {
+        yield return new UnityEngine.WaitForEndOfFrame();
+        string vendor_dir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
+        string cache_dir = Path.Combine(vendor_dir, ".upgrade");
+        foreach (var reference in referenceTaskQueue_)
+        {
+            foreach (var file in reference.files)
+            {
+                if (!File.Exists(Path.Combine(cache_dir, file.saveAs)))
+                    continue;
+                UnityLogger.Singleton.Info("Overwrite {0}", file.saveAs);
+                File.Copy(Path.Combine(cache_dir, file.saveAs), Path.Combine(vendor_dir, file.saveAs), true);
+                File.Delete(Path.Combine(cache_dir, file.saveAs));
+            }
+        }
+    }
+
+    private IEnumerator downloadDependencies(Schema _schema)
+    {
+        // 任何错误都不继续下载
+        if (errorCode != ErrorCode.OK)
+        {
+            yield break;
+        }
+
+        // 取一个未完成的任务
+        var task = referenceTaskQueue_.Find((_item) =>
+        {
+            return !_item.finished;
+        });
+
+        // 已经完成全部的下载
+        if (null == task)
+        {
+            yield break;
+        }
+
+        // 取一个未完成的文件
+        var file = task.files.Find((_item) =>
+        {
+            return !_item.finished;
+        });
+
+        // 如果任务的所有文件都已经下载完成，则任务完成
+        if (null == file)
+        {
+            task.finished = true;
+            yield return downloadDependencies(_schema);
+            yield break;
+        }
+
+        // 开始下载文件
+        updateEntryHash = file.hash;
+        fileWebRequest_ = UnityWebRequest.Get(file.url);
+        // 下载到缓存目录中
+        UnityLogger.Singleton.Trace("download {0}", file.saveAs);
+        string cache_dir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
+        cache_dir = Path.Combine(cache_dir, ".upgrade");
+        string saveAsPath = Path.Combine(cache_dir, file.saveAs);
+        fileWebRequest_.downloadHandler = new DownloadHandlerFile(saveAsPath);
+        yield return fileWebRequest_.SendWebRequest();
+        if (fileWebRequest_.result != UnityWebRequest.Result.Success)
+        {
+            UnityLogger.Singleton.Error(fileWebRequest_.error);
+            errorCode = ErrorCode.ENTRY_NETWORK_ERROR;
+            yield break;
+        }
+        if (file.size != fileWebRequest_.downloadedBytes)
+        {
+            errorCode = ErrorCode.ENTRY_SIZE_ERROR;
+            yield break;
+        }
+        // 保存文件的hash值
+        File.WriteAllText(saveAsPath + ".hash", file.hash);
+        updateFinishedSize += fileWebRequest_.downloadedBytes;
+        fileWebRequest_.Dispose();
+        fileWebRequest_ = null;
+        file.finished = true;
+        yield return downloadDependencies(_schema);
+    }
+
+    private string getPlatformSuffix()
+    {
+        string suffix = "";
+        switch (UnityEngine.Application.platform)
+        {
+            case UnityEngine.RuntimePlatform.WindowsPlayer:
+                suffix = "@windows";
+                break;
+            case UnityEngine.RuntimePlatform.WindowsEditor:
+                suffix = "@windows";
+                break;
+            case UnityEngine.RuntimePlatform.Android:
+                suffix = "@android";
+                break;
+            case UnityEngine.RuntimePlatform.WebGLPlayer:
+                suffix = "@webgl";
+                break;
+            case UnityEngine.RuntimePlatform.LinuxEditor:
+                suffix = "@linux";
+                break;
+            case UnityEngine.RuntimePlatform.LinuxPlayer:
+                suffix = "@linux";
+                break;
+            case UnityEngine.RuntimePlatform.OSXEditor:
+                suffix = "@osx";
+                break;
+            case UnityEngine.RuntimePlatform.OSXPlayer:
+                suffix = "@osx";
+                break;
+            case UnityEngine.RuntimePlatform.IPhonePlayer:
+                suffix = "@ios";
+                break;
+            default:
+                suffix = "";
+                break;
+        }
+        return suffix;
+    }
+}
--- FMP/Assets/Scripts/UpgradeBehaviour.cs
+++ FMP/Assets/Scripts/UpgradeBehaviour.cs
@@ -22,18 +22,34 @@ public class UpgradeBehaviour : MonoBehaviour
             public Text tip;
             public Button btnYes;
             public Button btnNo;
-
-            public string tipFormat;
         }
 
         [Serializable]
         public class PanelUpdating
         {
             public Transform root;
+            public Slider sliderTotal;
+            public Slider sliderSingle;
+            public Text textTip;
         }
 
         [Serializable]
-        public class PanelUpdateResult
+        public class PanelUpdateError
+        {
+            public Transform root;
+            public Text tip;
+            public Button btnRetry;
+            public Button btnSkip;
+        }
+
+        [Serializable]
+        public class PanelUpdateSuccess
+        {
+            public Transform root;
+        }
+
+        [Serializable]
+        public class PanelUpdateFailure
         {
             public Transform root;
         }
@@ -42,41 +58,88 @@ public class UpgradeBehaviour : MonoBehaviour
 
         public PanelUpdateTip updateTipPanel;
         public PanelUpdating updatingPanel;
-        public PanelUpdateResult updateResultPanel;
+        public PanelUpdateError updateErrorPanel;
+        public PanelUpdateSuccess updateSuccessPanel;
+        public PanelUpdateFailure updateFailurePanel;
+    }
+
+    public class UiTip
+    {
+        public string upgrade_parse_failure;
+        public string dependencies_update_tip;
+        public string dependencies_error;
+        public string downloading_tip;
     }
 
     public Ui ui;
+    public TextAsset upgradeTip;
+
+    public enum Panel
+    {
+        NONE,
+        TIP,
+        UPDATING,
+        ERROR,
+        FAILURE,
+        SUCCESS,
+    }
 
     private Upgrade.Schema schema_;
 
     private Upgrade upgrade_ = new Upgrade();
+    private UiTip uiTip_;
 
     private void Awake()
     {
         UnityLogger.Singleton.Info("########### Enter Upgrade Scene");
+        uiTip_ = JsonUtility.FromJson<UiTip>(upgradeTip.text);
+
         ui.root.gameObject.SetActive(true);
-        ui.updateTipPanel.root.gameObject.SetActive(false);
-        ui.updatingPanel.root.gameObject.SetActive(false);
-        ui.updateResultPanel.root.gameObject.SetActive(false);
-        ui.updateTipPanel.tipFormat = ui.updateTipPanel.tip.text;
+        ui.updateErrorPanel.btnSkip.onClick.AddListener(() =>
+        {
+            switchPanel(Panel.FAILURE);
+            enterStartup(3);
+        });
+        ui.updateErrorPanel.btnRetry.onClick.AddListener(() =>
+        {
+            StartCoroutine(updateDependencies());
+        });
+        ui.updateTipPanel.btnYes.onClick.AddListener(() =>
+        {
+            ui.updateTipPanel.root.gameObject.SetActive(false);
+            StartCoroutine(downloadDependencies());
+        });
+        ui.updateTipPanel.btnNo.onClick.AddListener(() =>
+        {
+            enterStartup(0);
+        });
+        ui.updatingPanel.sliderTotal.value = 0;
+        ui.updatingPanel.sliderSingle.value = 0;
+        ui.updatingPanel.textTip.text = "";
+
+        switchPanel(Panel.NONE);
     }
 
 
     private IEnumerator Start()
     {
-
         string vendorDir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
         string upgradeConfigFile = Path.Combine(vendorDir, "Upgrade.xml");
+
+        var xs = new XmlSerializer(typeof(Upgrade.Schema));
+        // 如果文件不存在，则创建默认的配置文件
         if (!File.Exists(upgradeConfigFile))
         {
-            UnityLogger.Singleton.Warning("{0} not found, skip upgrade", upgradeConfigFile);
-            SceneManager.LoadScene("startup");
-            yield break;
+            schema_ = new Upgrade.Schema();
+            using (FileStream writer = new FileStream(upgradeConfigFile, FileMode.CreateNew))
+            {
+                xs.Serialize(writer, schema_);
+                writer.Close();
+            }
         }
 
         try
         {
-            var xs = new XmlSerializer(typeof(Upgrade.Schema));
             using (FileStream reader = new FileStream(upgradeConfigFile, FileMode.Open))
             {
                 schema_ = xs.Deserialize(reader) as Upgrade.Schema;
@@ -84,31 +147,130 @@ public class UpgradeBehaviour : MonoBehaviour
         }
         catch (System.Exception ex)
         {
-            ui.updateResultPanel.root.gameObject.SetActive(true);
-            //TODO 显示错误
+            ui.updateErrorPanel.root.gameObject.SetActive(true);
+            ui.updateErrorPanel.tip.text = uiTip_.upgrade_parse_failure;
+
             UnityLogger.Singleton.Exception(ex);
             yield break;
         }
 
+        UnityLogger.Singleton.Info("Strategy of Update is {0}", schema_.body.update.strategy);
+
+        if (schema_.body.update.strategy.Equals("skip"))
+        {
+            UnityLogger.Singleton.Warning("skip upgrade");
+            enterStartup(0);
+            yield break;
+        }
+
+        // 升级流程参见设计文档中的升级流程说明
+        // !!! 升级操作会将文件下载到缓存目录中，如果所有文件下载成功，才会将缓存目录中的文件拷贝到虚拟环境中，
+        // 如果任何一个文件下载失败，虚拟环境中的文件不会发生变化
         upgrade_.ParseSchema(schema_);
         yield return updateDependencies();
     }
 
-    private IEnumerator updateDependencies()
+    private void Update()
     {
-        // 检查依赖
-        yield return upgrade_.CheckDependencies(schema_);
-        if (Upgrade.ErrorCode.OK != upgrade_.updateDependenciesError)
-        {
-            ui.updateResultPanel.root.gameObject.SetActive(true);
-            yield break;
-        }
-        ui.updateTipPanel.root.gameObject.SetActive(true);
-        ui.updateTipPanel.tip.text = string.Format(ui.updateTipPanel.tipFormat, formatSize(upgrade_.result.totalSize));
-        UnityLogger.Singleton.Info(upgrade_.result.totalSize.ToString());
+        ui.updatingPanel.textTip.text = string.Format(uiTip_.downloading_tip, upgrade_.updateEntryHash);
+        ui.updatingPanel.sliderTotal.value = upgrade_.updateTotalSize > 0 ? (upgrade_.updateFinishedSize * 100 / upgrade_.updateTotalSize) / 100f : 0;
+        ui.updatingPanel.sliderTotal.value = upgrade_.updateEntryProgress;
     }
 
-    private string formatSize(long _size)
+    private void enterStartup(float _delay)
+    {
+        StartCoroutine(delayEnterStartup(_delay));
+    }
+
+    private IEnumerator delayEnterStartup(float _delay)
+    {
+        yield return new WaitForSeconds(_delay);
+        SceneManager.LoadScene("startup");
+    }
+
+    private IEnumerator updateDependencies()
+    {
+        UnityLogger.Singleton.Info("check dependencies ......");
+        yield return upgrade_.CheckDependencies(schema_);
+        if (Upgrade.ErrorCode.OK != upgrade_.errorCode)
+        {
+            //检查阶段有错误
+            UnityLogger.Singleton.Error("check dependencies has error: {0}", upgrade_.errorCode.ToString());
+            if (schema_.body.update.strategy.Equals("manual"))
+            {
+                // 手动模式弹出错误提示
+                switchPanel(Panel.ERROR);
+                ui.updateErrorPanel.tip.text = string.Format(uiTip_.dependencies_error, upgrade_.errorCode.ToString());
+            }
+            else
+            {
+                switchPanel(Panel.FAILURE);
+                // 非手动模式进入startup场景
+                enterStartup(3);
+            }
+            yield break;
+        }
+
+        // 检查阶段没有错误
+        if (schema_.body.update.strategy.Equals("manual"))
+        {
+            // 没有数据需要更新
+            UnityLogger.Singleton.Info("ready to download dependencies, totalSize is {0}, finishedSize is {1}", upgrade_.updateTotalSize, upgrade_.updateFinishedSize);
+            if (upgrade_.updateFinishedSize >= upgrade_.updateTotalSize)
+            {
+                enterStartup(0);
+                yield break;
+            }
+            // 手动模式弹出升级提示
+            switchPanel(Panel.TIP);
+            ui.updateTipPanel.tip.text = string.Format(uiTip_.dependencies_update_tip, formatSize(upgrade_.updateTotalSize - upgrade_.updateFinishedSize));
+            yield break;
+        }
+
+        // 自动模式开始下载
+        yield return downloadDependencies();
+    }
+
+    private IEnumerator downloadDependencies()
+    {
+        UnityLogger.Singleton.Info("ready to download dependencies, totalSize is {0}, finishedSize is {1}", upgrade_.updateTotalSize, upgrade_.updateFinishedSize);
+        // 没有数据需要更新
+        if (upgrade_.updateFinishedSize >= upgrade_.updateTotalSize)
+        {
+            enterStartup(0);
+            yield break;
+        }
+
+        switchPanel(Panel.UPDATING);
+        yield return upgrade_.DownloadDependencies(schema_);
+        // 有错误
+        if (Upgrade.ErrorCode.OK != upgrade_.errorCode)
+        {
+            UnityLogger.Singleton.Error("download has error: {0}", upgrade_.errorCode.ToString());
+            if (schema_.body.update.strategy.Equals("manual"))
+            {
+                // 手动模式弹出错误提示
+                switchPanel(Panel.ERROR);
+                ui.updateErrorPanel.tip.text = string.Format(uiTip_.dependencies_error, upgrade_.errorCode.ToString());
+            }
+            else
+            {
+                switchPanel(Panel.FAILURE);
+                // 非手动模式进入startup场景
+                enterStartup(3);
+            }
+            yield break;
+        }
+
+        UnityLogger.Singleton.Info("all dependencies download success");
+        switchPanel(Panel.SUCCESS);
+        yield return new WaitForEndOfFrame();
+        yield return upgrade_.OverwriteDependencies(schema_);
+        UnityLogger.Singleton.Info("all dependencies overwrite success");
+        enterStartup(1);
+    }
+
+    private string formatSize(ulong _size)
     {
         if (_size < 1024)
             return string.Format("{0}B", _size);
@@ -117,5 +279,14 @@ public class UpgradeBehaviour : MonoBehaviour
         if (_size < 1024 * 1024 * 1024)
             return string.Format("{0}M", _size / 1024 / 1024);
         return string.Format("{0}G", _size / 1024 / 1024 / 1024);
+    }
+
+    private void switchPanel(Panel _panel)
+    {
+        ui.updateTipPanel.root.gameObject.SetActive(Panel.TIP == _panel);
+        ui.updatingPanel.root.gameObject.SetActive(Panel.UPDATING == _panel);
+        ui.updateSuccessPanel.root.gameObject.SetActive(Panel.SUCCESS == _panel);
+        ui.updateFailurePanel.root.gameObject.SetActive(Panel.FAILURE == _panel);
+        ui.updateErrorPanel.root.gameObject.SetActive(Panel.ERROR == _panel);
     }
 }
