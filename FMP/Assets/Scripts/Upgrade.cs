a1967a7c348fc3f37287e9d707c6e27ac0ada0e7
Easlee Liu <easlee@outlook.com>
2022-08-22 02:50:55 +0800

    update plugin

--- FMP/Assets/Scripts/Upgrade.cs
+++ FMP/Assets/Scripts/Upgrade.cs
@@ -11,7 +11,7 @@ using static UpgradeBehaviour;
 
 public class Upgrade
 {
-    public class MD5Json
+    public class Manifest
     {
         public class Entry
         {
@@ -92,25 +92,21 @@ public class Upgrade
         };
     }
 
-    public class FileTask
+    public class ManifestTask
     {
         public string url { get; set; }
-        public ulong size { get; set; }
-        public string hash { get; set; }
-        public string saveAs { get; set; }
         public bool finished { get; set; } = false;
     }
 
-    public class ReferenceTask
+    public class FileTask
     {
-        public string org { get; set; }
-        public string module { get; set; }
-        public string version { get; set; }
-        public string md5json_url { get; set; }
-        public List<FileTask> files = new List<FileTask>();
-        public bool parsed { get; set; } = false;
+        public string url { get; set; }
+        public string saveAs { get; set; }
+        public ulong size { get; set; }
+        public string hash { get; set; }
         public bool finished { get; set; } = false;
     }
+
 
     public enum ErrorCode
     {
@@ -137,49 +133,72 @@ public class Upgrade
         }
     }
 
-    private List<ReferenceTask> referenceTaskQueue_ = new List<ReferenceTask>();
+    private List<ManifestTask> manifestTasks_ = new List<ManifestTask>();
+    private List<FileTask> fileTasks_ = new List<FileTask>();
     private UnityWebRequest fileWebRequest_ = null;
 
     public void ParseSchema(Schema _schema)
     {
-        referenceTaskQueue_.Clear();
+        fileTasks_.Clear();
         foreach (var reference in _schema.body.update.fmp.refenrences)
         {
-            ReferenceTask task = new ReferenceTask();
             string version = reference.version;
             if (_schema.body.update.fmp.environment.Equals("develop"))
                 version = "develop";
 
             string address = string.Format("{0}/{1}/{2}@{3}", _schema.body.update.fmp.repository, reference.org, reference.module, version);
-            task.org = reference.org;
-            task.module = reference.module;
-            task.version = version;
-            task.md5json_url = string.Format("{0}/md5.json", address);
-            FileTask bridgeFileTask = new FileTask();
-            bridgeFileTask.url = string.Format("{0}/fmp-{1}-{2}-lib-bridge.dll", address, reference.org.ToLower(), reference.module.ToLower());
-            bridgeFileTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-bridge.dll", reference.org.ToLower(), reference.module.ToLower());
-            task.files.Add(bridgeFileTask);
-            FileTask protoFileTask = new FileTask();
-            protoFileTask.url = string.Format("{0}/fmp-{1}-{2}-lib-proto.dll", address, reference.org.ToLower(), reference.module.ToLower());
-            protoFileTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-proto.dll", reference.org.ToLower(), reference.module.ToLower());
-            task.files.Add(protoFileTask);
-            FileTask mvcsFileTask = new FileTask();
+
+            var manifestTask = new ManifestTask();
+            manifestTask.url = string.Format("{0}/md5.json", address);
+            manifestTasks_.Add(manifestTask);
+
+            var bridgeTask = new FileTask();
+            bridgeTask.url = string.Format("{0}/fmp-{1}-{2}-lib-bridge.dll", address, reference.org.ToLower(), reference.module.ToLower());
+            bridgeTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-bridge.dll", reference.org.ToLower(), reference.module.ToLower());
+            fileTasks_.Add(bridgeTask);
+
+            var protoTask = new FileTask();
+            protoTask.url = string.Format("{0}/fmp-{1}-{2}-lib-proto.dll", address, reference.org.ToLower(), reference.module.ToLower());
+            protoTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-proto.dll", reference.org.ToLower(), reference.module.ToLower());
+            fileTasks_.Add(protoTask);
+
+            var mvcsFileTask = new FileTask();
             mvcsFileTask.url = string.Format("{0}/fmp-{1}-{2}-lib-mvcs.dll", address, reference.org.ToLower(), reference.module.ToLower());
             mvcsFileTask.saveAs = string.Format("modules/fmp-{0}-{1}-lib-mvcs.dll", reference.org.ToLower(), reference.module.ToLower());
-            task.files.Add(mvcsFileTask);
-            FileTask unityFileTask = new FileTask();
+            fileTasks_.Add(mvcsFileTask);
+
+            var unityFileTask = new FileTask();
             unityFileTask.url = string.Format("{0}/{1}.FMP.MOD.{2}.LIB.Unity.dll", address, reference.org, reference.module);
             unityFileTask.saveAs = string.Format("modules/{0}.FMP.MOD.{1}.LIB.Unity.dll", reference.org, reference.module);
-            task.files.Add(unityFileTask);
-            FileTask uabFileTask = new FileTask();
-            uabFileTask.url = string.Format("{0}/{1}_{2}{3}.uab", address, reference.org.ToLower(), reference.module.ToLower(), getPlatformSuffix());
-            uabFileTask.saveAs = string.Format("uabs/{0}_{1}.uab", reference.org.ToLower(), reference.module.ToLower());
-            task.files.Add(uabFileTask);
-            FileTask xmlFileTask = new FileTask();
-            xmlFileTask.url = string.Format("{0}/{1}_{2}.xml", address, reference.org, reference.module);
-            xmlFileTask.saveAs = string.Format("configs/_{0}_{1}.xml", reference.org, reference.module);
-            task.files.Add(xmlFileTask);
-            referenceTaskQueue_.Add(task);
+            fileTasks_.Add(unityFileTask);
+
+            var uabTask = new FileTask();
+            uabTask.url = string.Format("{0}/{1}_{2}{3}.uab", address, reference.org.ToLower(), reference.module.ToLower(), getPlatformSuffix());
+            uabTask.saveAs = string.Format("uabs/{0}_{1}.uab", reference.org.ToLower(), reference.module.ToLower());
+            fileTasks_.Add(uabTask);
+
+            var xmlTask = new FileTask();
+            xmlTask.url = string.Format("{0}/{1}_{2}.xml", address, reference.org, reference.module);
+            xmlTask.saveAs = string.Format("configs/_{0}_{1}.xml", reference.org, reference.module);
+            fileTasks_.Add(xmlTask);
+        }
+
+        foreach (var plugin in _schema.body.update.fmp.plugins)
+        {
+            string version = plugin.version;
+            if (_schema.body.update.fmp.environment.Equals("develop"))
+                version = "develop";
+
+            string address = string.Format("{0}/__plugins__/{1}@{2}", _schema.body.update.fmp.repository, plugin.name, version);
+
+            var manifestTask = new ManifestTask();
+            manifestTask.url = string.Format("{0}/md5.json", address);
+            manifestTasks_.Add(manifestTask);
+
+            var dllTask = new FileTask();
+            dllTask.url = string.Format("{0}/{1}.dll", address, plugin.name);
+            dllTask.saveAs = string.Format("modules/{0}.dll", plugin.name);
+            fileTasks_.Add(dllTask);
         }
     }
 
@@ -193,15 +212,15 @@ public class Upgrade
         string cache_dir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
         cache_dir = Path.Combine(cache_dir, ".upgrade");
 
-        foreach (var task in referenceTaskQueue_)
+        foreach (var manifestTask in manifestTasks_)
         {
-            //解析过的包不需要再次清单文件
-            if (task.parsed)
+            //下载过的清单不需要再次下载
+            if (manifestTask.finished)
                 continue;
 
-            // 下载包的清单文件
-            UnityLogger.Singleton.Debug("pull {0}", task.md5json_url);
-            using (UnityWebRequest uwr = UnityWebRequest.Get(task.md5json_url))
+            // 下载清单文件
+            UnityLogger.Singleton.Debug("pull {0}", manifestTask.url);
+            using (UnityWebRequest uwr = UnityWebRequest.Get(manifestTask.url))
             {
                 uwr.downloadHandler = new DownloadHandlerBuffer();
                 yield return uwr.SendWebRequest();
@@ -214,10 +233,10 @@ public class Upgrade
                 // 解析清单文件
                 string text = uwr.downloadHandler.text;
 
-                MD5Json md5json = new MD5Json();
+                var manifest = new Manifest();
                 try
                 {
-                    md5json = JsonConvert.DeserializeObject<MD5Json>(text);
+                    manifest = JsonConvert.DeserializeObject<Manifest>(text);
                 }
                 catch (Exception ex)
                 {
@@ -226,51 +245,49 @@ public class Upgrade
                     yield break;
                 }
 
-                if (null == md5json)
+                if (null == manifest)
                 {
                     UnityLogger.Singleton.Error("manifest is null, maybe convert from json is failed");
                     errorCode = ErrorCode.MANIFEST_PARSE_ERROR;
                     yield break;
                 }
 
-                foreach (var filetask in task.files)
+                foreach (var entry in manifest.entries)
                 {
-                    var entry = md5json.entries.Find((_item) =>
+                    var file = fileTasks_.Find((_item) =>
                     {
-                        return _item.file.Equals(Path.GetFileName(filetask.url));
+                        return Path.GetFileName(_item.url).Equals(entry.file);
                     });
-                    // 任何一个需要更新的文件，如果在仓库中找不到，都报错
-                    if (null == entry)
-                    {
-                        errorCode = ErrorCode.ENTRY_NOTFOUNDINREPO;
-                        yield break;
-                    }
-                    filetask.size = entry.size;
-                    filetask.hash = entry.hash;
-                    filetask.finished = false;
+                    if (null == file)
+                        continue;
+                    file.size = entry.size;
+                    file.hash = entry.hash;
                     // 如果文件已经下载
-                    string hashFile = Path.Combine(cache_dir, filetask.saveAs) + ".hash";
+                    string hashFile = Path.Combine(cache_dir, file.saveAs) + ".hash";
                     if (File.Exists(hashFile))
                     {
-                        if (File.ReadAllText(hashFile).Equals(filetask.hash))
+                        if (File.ReadAllText(hashFile).Equals(file.hash))
                         {
-                            filetask.finished = true;
+                            file.finished = true;
                         }
                     }
                 }
-                task.parsed = true;
             }
         }
 
         // 重新计算需要下载总量和已下载量
-        foreach (var reference in referenceTaskQueue_)
+        foreach (var fileTask in fileTasks_)
         {
-            foreach (var file in reference.files)
+            // 任何一个需要更新的文件，如果在仓库中找不到，都报错
+            if (string.IsNullOrEmpty(fileTask.hash))
             {
-                updateTotalSize += file.size;
-                if (file.finished)
-                    updateFinishedSize += file.size;
+                errorCode = ErrorCode.ENTRY_NOTFOUNDINREPO;
+                yield break;
             }
+
+            updateTotalSize += fileTask.size;
+            if (fileTask.finished)
+                updateFinishedSize += fileTask.size;
         }
     }
 
@@ -285,16 +302,13 @@ public class Upgrade
         yield return new UnityEngine.WaitForEndOfFrame();
         string vendor_dir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
         string cache_dir = Path.Combine(vendor_dir, ".upgrade");
-        foreach (var reference in referenceTaskQueue_)
+        foreach (var task in fileTasks_)
         {
-            foreach (var file in reference.files)
-            {
-                if (!File.Exists(Path.Combine(cache_dir, file.saveAs)))
-                    continue;
-                UnityLogger.Singleton.Info("Overwrite {0}", file.saveAs);
-                File.Copy(Path.Combine(cache_dir, file.saveAs), Path.Combine(vendor_dir, file.saveAs), true);
-                File.Delete(Path.Combine(cache_dir, file.saveAs));
-            }
+            if (!File.Exists(Path.Combine(cache_dir, task.saveAs)))
+                continue;
+            UnityLogger.Singleton.Info("Overwrite {0}", task.saveAs);
+            File.Copy(Path.Combine(cache_dir, task.saveAs), Path.Combine(vendor_dir, task.saveAs), true);
+            File.Delete(Path.Combine(cache_dir, task.saveAs));
         }
     }
 
@@ -307,7 +321,7 @@ public class Upgrade
         }
 
         // 取一个未完成的任务
-        var task = referenceTaskQueue_.Find((_item) =>
+        var task = fileTasks_.Find((_item) =>
         {
             return !_item.finished;
         });
@@ -318,28 +332,14 @@ public class Upgrade
             yield break;
         }
 
-        // 取一个未完成的文件
-        var file = task.files.Find((_item) =>
-        {
-            return !_item.finished;
-        });
-
-        // 如果任务的所有文件都已经下载完成，则任务完成
-        if (null == file)
-        {
-            task.finished = true;
-            yield return downloadDependencies(_schema);
-            yield break;
-        }
-
         // 开始下载文件
-        updateEntryHash = file.hash;
-        fileWebRequest_ = UnityWebRequest.Get(file.url);
+        updateEntryHash = task.hash;
+        fileWebRequest_ = UnityWebRequest.Get(task.url);
         // 下载到缓存目录中
-        UnityLogger.Singleton.Trace("download {0}", file.saveAs);
+        UnityLogger.Singleton.Trace("download {0}", task.saveAs);
         string cache_dir = Path.Combine(Constant.DataPath, VendorManager.Singleton.active);
         cache_dir = Path.Combine(cache_dir, ".upgrade");
-        string saveAsPath = Path.Combine(cache_dir, file.saveAs);
+        string saveAsPath = Path.Combine(cache_dir, task.saveAs);
         fileWebRequest_.downloadHandler = new DownloadHandlerFile(saveAsPath);
         yield return fileWebRequest_.SendWebRequest();
         if (fileWebRequest_.result != UnityWebRequest.Result.Success)
@@ -348,17 +348,17 @@ public class Upgrade
             errorCode = ErrorCode.ENTRY_NETWORK_ERROR;
             yield break;
         }
-        if (file.size != fileWebRequest_.downloadedBytes)
+        if (task.size != fileWebRequest_.downloadedBytes)
         {
             errorCode = ErrorCode.ENTRY_SIZE_ERROR;
             yield break;
         }
         // 保存文件的hash值
-        File.WriteAllText(saveAsPath + ".hash", file.hash);
+        File.WriteAllText(saveAsPath + ".hash", task.hash);
         updateFinishedSize += fileWebRequest_.downloadedBytes;
         fileWebRequest_.Dispose();
         fileWebRequest_ = null;
-        file.finished = true;
+        task.finished = true;
         yield return downloadDependencies(_schema);
     }
 
